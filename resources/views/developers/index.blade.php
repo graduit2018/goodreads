cb705debdda2967263da924d95539dc63843cac5
ToolOwner <toolowner@hotmail.com>
2019-02-19 11:55:38 +0700

    generate personal access tokens

--- resources/views/developers/index.blade.php
+++ resources/views/developers/index.blade.php
@@ -12,6 +12,12 @@
             <div class="mb-4">
                 <passport-authorized-clients></passport-authorized-clients>
             </div>
+            <br>
+            <p class="h5 text-center">Create access tokens for quick testing</p>
+            <br>
+            <div class="mb-4">
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+            </div>
         </div>
     </div>
 </div>
